ac1c347695d36732c85a7785b27b4115b746459d
Nabil Islam Hamza <hamjanail11@gmail.com>
2025-11-03 16:00:55 +0600

    dploy error

--- src/components/dashboard/category/CreateCategory.jsx
+++ src/components/dashboard/category/CreateCategory.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
 import {
     Field,
     FieldDescription,
@@ -25,7 +26,7 @@ export default function CreateCategory() {
     const {
         register,
         handleSubmit,
-        watch,
+        reset,
         formState: { errors },
     } = useForm()
     console.log(errors);
@@ -35,64 +36,87 @@ export default function CreateCategory() {
         console.log(data);
         try {
             axios.post("http://localhost:3000/api/v1/category/create-category", data)
+            toast.success("✅ Category created successfully!")
+            reset();
 
         } catch (error) {
             console.log(error);
 
+            toast.error("❌ Something went wrong!")
 
         }
     }
+    const handleCancel = () => {
+        reset();
+        toast.info("Form cleared!");
+    }
     return (
-        <div className="w-full max-w-md ">
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <FieldGroup>
-                    <FieldSet>
-                        <FieldLegend>Create Category</FieldLegend>
-                        <FieldGroup>
-                            <Field>
-                                <FieldLabel htmlFor="checkout-7j9-card-name-43j">
-                                    Name
-                                </FieldLabel>
-                                <Input
-                                    id="name"
-                                    placeholder="Name of Category "
-                                    {...register("name", { required: "Category name is required" })}
-                                />
-                                {errors.name && (
-                                    <p className=" text-red-500">{errors.name.message}</p>
-                                )}
+        <>
+            <div className="w-full max-w-md ">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <FieldGroup>
+                        <FieldSet>
+                            <FieldLegend>Create Category</FieldLegend>
+                            <FieldGroup>
+                                <Field>
+                                    <FieldLabel htmlFor="checkout-7j9-card-name-43j">
+                                        Name
+                                    </FieldLabel>
+                                    <Input
+                                        id="name"
+                                        placeholder="Name of Category "
+                                        {...register("name", { required: "Category name is required" })}
+                                    />
+                                    {errors.name && (
+                                        <p className=" text-red-500">{errors.name.message}</p>
+                                    )}
 
-                            </Field>
+                                </Field>
 
-                        </FieldGroup>
-                    </FieldSet>
+                            </FieldGroup>
+                        </FieldSet>
 
 
-                    <FieldSet>
-                        <FieldGroup>
-                            <Field>
-                                <FieldLabel htmlFor="checkout-7j9-optional-comments">
-                                    Description
-                                </FieldLabel>
-                                <Textarea
-                                    id="description"
-                                    placeholder="Add category description"
-                                    {...register("description", { required: "Category description is required" })}
-                                />
-                                {errors.description && (
-                                    <p className=" text-red-500">{errors.description.message}</p>
-                                )}
-                            </Field>
-                        </FieldGroup>
-                    </FieldSet>
-                    <Field orientation="horizontal">
-                        <Button className="bg-red-400 hover:bg-red-500 text-white dark:bg-red-700 dark:hover:bg-red-800" type="submit">Submit</Button>
-                        <Button className="bg-black hover:bg-gray-800 text-white  ">
-                            Cancel
-                        </Button>
-                    </Field>
-                </FieldGroup>
-            </form>
-        </div >
+                        <FieldSet>
+                            <FieldGroup>
+                                <Field>
+                                    <FieldLabel htmlFor="checkout-7j9-optional-comments">
+                                        Description
+                                    </FieldLabel>
+                                    <Textarea
+                                        id="description"
+                                        placeholder="Add category description"
+                                        {...register("description", { required: "Category description is required" })}
+                                    />
+                                    {errors.description && (
+                                        <p className=" text-red-500">{errors.description.message}</p>
+                                    )}
+                                </Field>
+                            </FieldGroup>
+                        </FieldSet>
+                        <Field orientation="horizontal">
+                            <Button className="bg-red-400 hover:bg-red-500 text-white dark:bg-red-700 dark:hover:bg-red-800" type="submit">Create</Button>
+                            <Button onClick={handleCancel} className="bg-black hover:bg-gray-800 text-white  ">
+                                Cancel
+                            </Button>
+                        </Field>
+                    </FieldGroup>
+                </form>
+            </div >
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+
+            />
+
+        </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { RouterProvider } from "react-router/dom";
 import CreateProduct from './components/dashboard/product/CreateProduct.jsx';
 import CreateCategory from './components/dashboard/category/CreateCategory.jsx';
 import DashboardLayout from './components/dashboard/DashboardLayout.jsx';
-import { DashboardHome } from './components/dashboard/home/dashboardHome.jsx';
+import DashboardHome from './components/dashboard/home/DashboardHome.jsx';
 import AllCategory from './components/dashboard/category/allCategory.jsx';
 import UpdateCategory from './components/dashboard/category/UpdateCategory.jsx';
 
