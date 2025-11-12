3ef5ca245c2afd75327d676af733d5ac6e2b7e59
Nabil Islam Hamza <hamjanail11@gmail.com>
2025-11-13 05:05:50 +0600

    create product dashboad

--- src/components/dashboard/product/CreateProduct.jsx
+++ src/components/dashboard/product/CreateProduct.jsx
@@ -16,15 +16,87 @@ import {
     SelectItem,
     SelectTrigger,
     SelectValue,
+    SelectLabel,
+    SelectGroup,
 } from "@/components/ui/select"
+
 import { Textarea } from "@/components/ui/textarea"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 
 export default function CreateProduct() {
+    const [category, setCategory] = useState([])
+    const [subCategory, setSubCategory] = useState([])
+    const [fromData, setFromdata] = useState({
+        name: "",
+        description: "",
+        price: "",
+        review: "",
+        stock: "",
+        discount: "",
+        quantity: "",
+        sold: "",
+        image: null,
+        reting: "",
+        category: "",
+        subcategory: "",
+    })
+    const fetchSubCategory = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:3000/api/v1/subcategory/get-allsubcategory")
+            setSubCategory(data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const fetchCategory = async () => {
+        try {
+            const { data } = await axios.get("http://localhost:3000/api/v1/category/get-allcategories")
+            setCategory(data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        fetchCategory()
+        fetchSubCategory()
+    }, [])
+    console.log(category);
+    console.log(subCategory);
+
+    const handleFileChange = (e, file) => {
+        console.log(e);
+        console.log(file.files[0]);
+        setFromdata((prev) => ({ ...prev, "image": file.files[0] }))
+
+    }
+    const handleChange = (field, value) => {
+        setFromdata((prev) => ({ ...prev, [field]: value }))
+
+
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log(fromData);
+        const dataToSend = new FormData();
+        Object.entries(fromData).forEach(([key, value]) => {
+            dataToSend.append(key, value)
+        })
+        await axios.post("http://localhost:3000/api/v1/product/create-product",
+            dataToSend,
+            {
+                headers: {
+
+                    "Content-Type": `multipart/form-data`,
+                },
+            })
+
+    }
     return (
         <div className="w-full p-5">
-
-            <form>
+            <form onSubmit={handleSubmit}>
                 <FieldGroup>
                     <FieldSet>
                         <FieldLegend>Create Product</FieldLegend>
@@ -32,14 +104,15 @@ export default function CreateProduct() {
                             All Product create by this.
                         </FieldDescription>
                         <div className="flex gap-x-20">
-
                             <FieldGroup>
                                 <Field>
-                                    <FieldLabel htmlFor="checkout-7j9-card-name-43j">
+                                    <FieldLabel htmlFor="name">
                                         Product Name
                                     </FieldLabel>
                                     <Input
-                                        id="checkout-7j9-card-name-43j"
+                                        id="name"
+                                        onChange={(e) => handleChange("name", e.target.value)}
+                                        value={fromData.name}
                                         placeholder="Name of Category "
                                         required
                                     />
@@ -47,49 +120,163 @@ export default function CreateProduct() {
                             </FieldGroup>
                             <FieldGroup>
                                 <Field>
-                                    <FieldLabel htmlFor="checkout-7j9-card-name-43j">
+                                    <FieldLabel htmlFor="price">
                                         Product Price
                                     </FieldLabel>
                                     <Input
-                                        id="checkout-7j9-card-name-43j"
+                                        id="price"
+                                        onChange={(e) => handleChange("price", e.target.value)}
+                                        value={fromData.price}
                                         placeholder="Product Price"
                                         required
                                     />
                                 </Field>
-
                             </FieldGroup>
                             <FieldGroup>
                                 <Field>
-                                    <FieldLabel htmlFor="checkout-7j9-card-name-43j">
+                                    <FieldLabel htmlFor="mage">
                                         Product Image
                                     </FieldLabel>
                                     <Input
-                                        id="checkout-7j9-card-name-43j"
-                                        placeholder="Name of Category "
+                                        id="image"
+                                        onChange={(e) => handleFileChange("image", e.target)}
+                                        placeholder="image upload "
                                         required
-
+                                        type="file"
                                     />
                                 </Field>
-
                             </FieldGroup>
                         </div>
                     </FieldSet>
-
+                    <FieldSet>
+                        <FieldGroup>
+                            <Field className="w-[600px]">
+                                <FieldLabel htmlFor="stock">
+                                    Product Stock
+                                </FieldLabel>
+                                <Textarea
+                                    id="stock"
+                                    onChange={(e) => handleChange("stock", e.target.value)}
+                                    value={fromData.stock}
+                                    placeholder="Add Product stock"
+                                    className="resize-none  "
+                                />
+                            </Field>
+                        </FieldGroup>
+                    </FieldSet>
+                    <FieldSet>
+                        <FieldGroup>
+                            <Field className="w-[600px]">
+                                <FieldLabel htmlFor="quantity">
+                                    Product Quantity
+                                </FieldLabel>
+                                <Textarea
+                                    id="quantity"
+                                    onChange={(e) => handleChange("quantity", e.target.value)}
+                                    value={fromData.quantity}
+                                    placeholder="Add Product quantity"
+                                    className="resize-none  "
+                                />
+                            </Field>
+                        </FieldGroup>
+                    </FieldSet>
+                    <FieldSet>
+                        <FieldGroup>
+                            <Field className="w-[600px]">
+                                <FieldLabel htmlFor="reting">
+                                    Product Reting
+                                </FieldLabel>
+                                <Textarea
+                                    id="reting"
+                                    onChange={(e) => handleChange("reting", e.target.value)}
+                                    value={fromData.reting}
+                                    placeholder="Add Product reting"
+                                    className="resize-none  "
+                                />
+                            </Field>
+                        </FieldGroup>
+                    </FieldSet>
 
                     <FieldSet>
                         <FieldGroup>
                             <Field className="w-[600px]">
-                                <FieldLabel htmlFor="checkout-7j9-optional-comments">
+                                <FieldLabel htmlFor="discount">
+                                    Product Discount
+                                </FieldLabel>
+                                <Textarea
+                                    id="discount"
+                                    onChange={(e) => handleChange("discount", e.target.value)}
+                                    value={fromData.discount}
+                                    placeholder="Add Product discount"
+                                    className="resize-none"
+                                />
+                            </Field>
+                        </FieldGroup>
+                    </FieldSet>
+                    <FieldSet>
+                        <FieldGroup>
+                            <Field className="w-[600px]">
+                                <FieldLabel htmlFor="description">
                                     Product Description
                                 </FieldLabel>
                                 <Textarea
-                                    id="checkout-7j9-optional-comments"
+                                    id="description"
+                                    onChange={(e) => handleChange("description", e.target.value)}
+                                    value={fromData.description}
                                     placeholder="Add Product description"
                                     className="resize-none  "
                                 />
                             </Field>
                         </FieldGroup>
                     </FieldSet>
+                    <FieldGroup className="my-2" >
+                        <Field>
+                            <FieldLabel htmlFor="category">
+                                Product category
+                            </FieldLabel>
+                            <Select
+                                id="category"
+                                onValueChange={(value) => handleChange("category", value)}
+                                value={fromData.category} >
+                                <SelectTrigger >
+                                    <SelectValue placeholder="Select Category" />
+                                </SelectTrigger>
+                                <SelectContent className='my-5'>
+                                    <SelectGroup>
+                                        {
+                                            category.map((cat) => {
+                                                return <SelectItem value={cat._id}> {cat.name} </SelectItem>
+                                            })
+                                        }
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </Field>
+                    </FieldGroup >
+                    <FieldGroup className="my-2" >
+                        <Field>
+                            <FieldLabel htmlFor="subcategory">
+                                Product Subcategory
+                            </FieldLabel>
+                            <Select
+                                id="subcategory"
+                                onValueChange={(value) => handleChange("subcategory", value)}
+                                value={fromData.subcategory} >
+                                <SelectTrigger >
+                                    <SelectValue placeholder="Select subCategory" />
+                                </SelectTrigger>
+                                <SelectContent className='my-5'>
+                                    <SelectGroup>
+                                        {
+                                            subCategory.map((subCategory) => {
+                                                return <SelectItem value={subCategory._id}> {subCategory.name} </SelectItem>
+                                            })
+                                        }
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </Field>
+                    </FieldGroup >
                     <Field orientation="horizontal">
                         <Button className="bg-red-400 hover:bg-red-500 text-white dark:bg-red-700 dark:hover:bg-red-800" type="submit">Submit</Button>
                         <Button className="bg-black hover:bg-gray-800 text-white  ">
--- src/components/dashboard/subcategory/UpdateSubcategory.jsx
+++ src/components/dashboard/subcategory/UpdateSubcategory.jsx
@@ -25,6 +25,8 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { useParams } from 'react-router';
 import { useForm } from 'react-hook-form';
+import { ToastContainer, toast } from 'react-toastify';
+
 
 const UpdateSubcategory = () => {
 
@@ -76,97 +78,122 @@ const UpdateSubcategory = () => {
         e.preventDefault();
         try {
             await axios.patch(`http://localhost:3000/api/v1/subcategory/update-subcategory/${id}`, subCategory)
-
+            toast.success("✅ Subcategory updated successfully!")
+            setSubCategory({ name: "", description: "", category: "" })
         } catch (error) {
             console.log(error);
+            toast.error("❌ Something went wrong!")
 
 
         }
 
     }
+    const handleCancel = () => {
+        setSubCategory({
+            name: "",
+            description: "",
+            category: "",
+        })
+        toast.info()
+    }
     return (
-        <div>
-            <div className="w-full max-w-md ">
-                <form >
-                    <FieldGroup>
-                        <FieldSet>
-                            <FieldLegend>Update Subcategory</FieldLegend>
-                            <FieldGroup className="my-2">
+        <>
+            <div>
+                <div className="w-full max-w-md ">
+                    <form >
+                        <FieldGroup>
+                            <FieldSet>
+                                <FieldLegend>Update Subcategory</FieldLegend>
+                                <FieldGroup className="my-2">
+                                    <Field>
+                                        <FieldLabel htmlFor="checkout-7j9-card-name-43j">
+                                            Name
+                                        </FieldLabel>
+                                        <Input
+                                            onChange={(e) => setSubCategory({
+                                                ...subCategory, name: e.target.value
+                                            })}
+                                            id="name"
+
+                                            placeholder="Name of Subcategory "
+                                            value={subCategory?.name}
+
+                                        />
+
+
+                                    </Field>
+
+                                </FieldGroup>
+                            </FieldSet>
+
+
+                            <FieldSet>
+                                <FieldGroup className="my-2">
+                                    <Field>
+                                        <FieldLabel htmlFor="checkout-7j9-optional-comments">
+                                            Description
+                                        </FieldLabel>
+                                        <Input
+                                            onChange={(e) => setSubCategory({
+                                                ...subCategory, description: e.target.value
+                                            })}
+                                            id="description"
+                                            placeholder="Add Subcategory description"
+                                            value={subCategory?.description}
+
+                                        />
+
+                                    </Field>
+                                </FieldGroup>
+                            </FieldSet>
+                            <FieldGroup className="my-2" >
                                 <Field>
-                                    <FieldLabel htmlFor="checkout-7j9-card-name-43j">
-                                        Name
-                                    </FieldLabel>
-                                    <Input
-                                        onChange={(e) => setSubCategory({
-                                            ...subCategory, name: e.target.value
-                                        })}
-                                        id="name"
+                                    <Select onValueChange={(value) => setSubCategory({ ...subCategory, category: value, })} >
+                                        <SelectTrigger >
+                                            <SelectValue placeholder="Select Category" />
+                                        </SelectTrigger>
+                                        <SelectContent className='my-5'>
+                                            <SelectGroup>
 
-                                        placeholder="Name of Subcategory "
-                                        value={subCategory?.name}
+                                                {
 
-                                    />
+                                                    category.map((cat) => {
+                                                        return <SelectItem value={cat._id}> {cat.name} </SelectItem>
+                                                    })
 
+                                                }
 
+                                            </SelectGroup>
+
+                                        </SelectContent>
+                                    </Select>
                                 </Field>
+                            </FieldGroup >
 
-                            </FieldGroup>
-                        </FieldSet>
-
-
-                        <FieldSet>
-                            <FieldGroup className="my-2">
-                                <Field>
-                                    <FieldLabel htmlFor="checkout-7j9-optional-comments">
-                                        Description
-                                    </FieldLabel>
-                                    <Input
-                                        onChange={(e) => setSubCategory({
-                                            ...subCategory, description: e.target.value
-                                        })}
-                                        id="description"
-                                        placeholder="Add Subcategory description"
-                                        value={subCategory?.description}
-
-                                    />
-
-                                </Field>
-                            </FieldGroup>
-                        </FieldSet>
-                        <FieldGroup className="my-2" >
-                            <Field>
-                                <Select onValueChange={(value) => setSubCategory({ ...setSubCategory, category: value, })} >
-                                    <SelectTrigger >
-                                        <SelectValue placeholder="Select Category" />
-                                    </SelectTrigger>
-                                    <SelectContent className='my-5'>
-                                        <SelectGroup>
-
-                                            {
-
-                                                category.map((cat) => {
-                                                    return <SelectItem value={cat._id}> {cat.name} </SelectItem>
-                                                })
-
-                                            }
-
-                                        </SelectGroup>
-
-                                    </SelectContent>
-                                </Select>
+                            <Field orientation="horizontal">
+                                <Button type="submit" onClick={handleUpdate} className="bg-red-400 hover:bg-red-500 text-white dark:bg-red-700 dark:hover:bg-red-800" >Submit</Button>
+                                <Button onClick={handleCancel} className="bg-black hover:bg-gray-800 text-white  ">
+                                    Cancel
+                                </Button>
                             </Field>
-                        </FieldGroup >
+                        </FieldGroup>
+                    </form>
+                </div >
+            </div>
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
 
-                        <Field orientation="horizontal">
-                            <Button type="submit" onClick={handleUpdate} className="bg-red-400 hover:bg-red-500 text-white dark:bg-red-700 dark:hover:bg-red-800" >Submit</Button>
-                            <Button className="bg-black hover:bg-gray-800 text-white  ">
-                                Cancel
-                            </Button>
-                        </Field>
-                    </FieldGroup>
-                </form>
-            </div >
-        </div>
+            />
+        </>
     )
 }
 
